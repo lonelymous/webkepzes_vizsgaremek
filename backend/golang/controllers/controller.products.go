2d8cb6e5eaeca2d92b28b9c798627b671afbb56d
Szabó Ármin András <46499638+lonelymous@users.noreply.github.com>
2023-05-07 16:11:49 +0200

    /api/products
    GET and POST on /api/products

--- backend/golang/controllers/controller.products.go
+++ backend/golang/controllers/controller.products.go
@@ -1,7 +1,32 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"main/models"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Products_Controller(ctx *fiber.Ctx) error {
-	return nil
+	var err error
+
+	switch ctx.Method() {
+	case "GET":
+		products, err := Get_Products()
+		if err != nil {
+			return err
+		}
+
+		return ctx.JSON(products)
+	case "POST":
+		var product models.ProductEditor
+
+		err = ctx.BodyParser(&product)
+		if err != nil {
+			return err
+		}
+
+		return Update_Product(product)
+	}
+
+	return err
 }
--- backend/golang/controllers/products.go
+++ backend/golang/controllers/products.go
@@ -1,1 +1,38 @@
 package controllers
+
+import (
+	"main/models"
+
+	"github.com/lonelymous/godab"
+)
+
+func Get_Products() ([]models.Product, error) {
+
+	result, err := godab.Queryx("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+
+	var products []models.Product
+	for result.Next() {
+		var product models.Product
+		err = result.StructScan(&product)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	return products, nil
+}
+
+func Update_Product(product models.ProductEditor, productId ...int) error {
+	var err error
+
+	if len(productId) > 0 {
+		_, err = godab.Exec("UPDATE products SET name=?, description=?, price=?, quantity=? WHERE id=?", product.Name, product.Description, product.Price, product.Quantity, productId[0])
+	} else {
+		_, err = godab.Exec("INSERT INTO products (name, description, price, quantity) VALUES (?,?,?,?)", product.Name, product.Description, product.Price, product.Quantity)
+	}
+
+	return err
+}
